1405ac408593f42a1fd15a80992a2d25985b8bc3
cratesium <cratesium@gmail.com>
2026-04-07 00:34:59 +0530

    learned singleton pattern

---
+++ SingletonDesignPattern/Caller.java
@@ -0,0 +1,29 @@
+package SingletonDesignPattern;
+
+public class Caller {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hiii Lets starts singlton class object creation ");
+
+        try{
+            
+          //  SingleTonClass singleTonClassObjectCreationFirstTry = new SingleTonClass(); //Error 
+
+            SingleTonClass singleTonClassObjectCreationSecondTry = SingleTonClass.getSingleTonClassObject();
+            System.out.println(singleTonClassObjectCreationSecondTry.name);
+
+
+            System.out.println("--------------->>>>>>> sepeartion bw both object <<<<<<<<--------------");
+
+
+            SingleTonClass singleTonClassObjectCreationThiordTry = SingleTonClass.getSingleTonClassObject();
+            System.out.println(singleTonClassObjectCreationThiordTry.name);
+
+
+        }catch(Exception e){
+           System.out.println(e);
+        }
+    }
+    
+}
---
+++ SingletonDesignPattern/SingleTonClass.java
@@ -0,0 +1,33 @@
+package SingletonDesignPattern;
+
+public class SingleTonClass {
+    String name ;
+
+    // we are needed to allow SingleTonClass to have only one object at any cost 
+    // remove the public constructor to block creation of new object 
+    // make the object private so it cant be accessed from the outside 
+    // create a get method to return  the object by checking the codition that create new object only if object is null otherwise return the current one 
+
+    private static SingleTonClass singleTonClassObj;
+
+
+    private SingleTonClass(){
+
+    }
+
+    public static SingleTonClass getSingleTonClassObject(){
+
+        if (singleTonClassObj==null) {
+
+            singleTonClassObj= new SingleTonClass();
+            singleTonClassObj.name="Shikhar";
+            
+        }
+        return singleTonClassObj;
+
+
+    }
+
+    
+    
+}
