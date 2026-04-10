9b763f676cae902064872aad42d365c1af3370c6
cratesium <cratesium@gmail.com>
2026-04-10 23:17:57 +0530

    changes

--- SingletonDesignPattern/ThreadSafe/synchronisedMthodKeyword/SingleTonClassV2.java
+++ SingletonDesignPattern/ThreadSafe/synchronisedMthodKeyword/SingleTonClassV2.java
@@ -10,7 +10,7 @@ public class SingleTonClassV2 {
     private static SingleTonClassV2  SingleTonClassV2 = new SingleTonClassV2();
 
    //EAGER WAY OF CREATING OBJECT created at start time 
-    // no synchro
+    // no synchronization is needed as the object is created at class load time and there is no chance of multiple threads creating multiple objects
     public static  SingleTonClassV2 getObject(){
         return SingleTonClassV2;
     }
